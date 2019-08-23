668a96d87267930071875a4b234dbeeda15a390b
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2019-08-23 15:42:00 -0500

    added more aliases

---
+++ custom-configs/patrick/custom_aliases.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+alias stash='git stash'
+alias pop='git stash pop'
+alias status='git status'
+alias pipi='pip install -r requirements/base.txt'
+alias pipu='pip install --upgrade pip'
