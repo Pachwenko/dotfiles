2b40f8d22b8892ecc5c4d2745f89ec259f290dc9
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2019-09-03 08:00:36 -0500

    Added another alias for git

--- custom-configs/patrick/custom_aliases.sh
+++ custom-configs/patrick/custom_aliases.sh
@@ -3,5 +3,6 @@
 alias stash='git stash'
 alias pop='git stash pop'
 alias status='git status'
+alias s='git status'
 alias pipi='pip install -r requirements/base.txt'
 alias pipu='pip install --upgrade pip'
