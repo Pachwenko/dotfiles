a7130694b9032e23cbf8f3bca781f3d9f1bc7166
Patrick McCartney <patrick.mccartney@imtapps.com>
2021-03-15 13:39:41 -0500

    Add neovim stuff

--- custom-configs/my-custom-configs/shell_modifiers.sh
+++ custom-configs/my-custom-configs/shell_modifiers.sh
@@ -9,9 +9,13 @@ source "$HOME/.cargo/env"
 # add DOOM Emacs to path
 PATH="$HOME/.emacs.d/bin:$PATH"
 
-# use ripgrep instead of grep
+EDITOR="nvim"
+VISUAL="$EDITOR"
+GIT_EDITOR="$EDITOR"
+
+# use ripgrep instead of grep (way faster) - install with brew install ripgrep
 alias grep='rg -i'
-# ag is ues for silver searcher
+# ag is used for silver searcher, also use ripgrep instead
 alias ag='rg'
 
 # The usual  `pyenv init` stuff
@@ -21,5 +25,7 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi
 
 # fix python install errors
-export MACOSX_DEPLOYMENT_TARGET="11.2.2"
+# export MACOSX_DEPLOYMENT_TARGET="11.2.2" # not sure why this happens but it can become "11.2."...
 export CFLAGS=-"I/usr/local/include -L/usr/local/lib"
+export LDFLAGS="-L/usr/local/opt/readline/lib"
+export CPPFLAGS="-I/usr/local/opt/readline/include"
--- custom-configs/my-custom-configs/vim-modifiers.vim
+++ custom-configs/my-custom-configs/vim-modifiers.vim
@@ -16,3 +16,21 @@ set backupcopy=yes
 
 "Meaningful backup name
 au BufWritePre * let &bex = '@' . strftime("%F.%H:%M:%S")
+
+" pretty print
+command Jsontool :%! python -m json.tool %
+command! PrettyPrintJSON %!python -m json.tool
+command! PrettyPrintHTML !tidy -mi -html -wrap 0 %
+command! PrettyPrintXML !tidy -mi -xml -wrap 0 %
+command! RemoveEmptyLines :g/^\s*$/d
+" More vertical and Horizon split commands
+" Vertical split is :sp and Horizontal split is :vs
+command Vs :vs
+command Hs :sp
+command VS :vs
+command HS :sp
+
+command! FormatPython 0,$!black -l 120
+
+autocmd FileType python nnoremap <leader>y :0,$!yapf --style="{based_on_style: facebook, FORCE_MULTILINE_DICT: true, SPLIT_ALL_COMMA_SEPARATED_VALUES: true, COLUMN_LIMIT: 120, BLANK_LINE_BEFORE_NESTED_CLASS_OR_DEF: true}"<Cr><C-o>
+
